0f29b4f72aff10192f193c6f579b46eed5eef447
gabriel benatar <gabriel.benatar@beityaacov.com.br>
2019-10-16 15:51:46 -0300

    dando cartas ao crupier

--- EP_Blackjack.py
+++ EP_Blackjack.py
@@ -5,21 +5,16 @@ Created on Wed Oct 16 10:13:48 2019
 
 @author: gabrielsalvatorbenatar
 """
-cartas = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A']
-#já que a probabilidade de sair uma das cartas se só hover uma delas ou se hover 4 de cada uma, seria mais fácil deixar uma de cada
-cartas[0]=1
-cartas[1]=2
-cartas[2]=3
-cartas[4]=5
-cartas[3]=4
-cartas[5]=6
-cartas[6]=7
-cartas[7]=8
-cartas[8]=9
-cartas[9]=10
-cartas[10]=10
-cartas[11]=10
-cartas[12]=10
+import random
 
+
+c_cartas = []#crupier / computador
+u_cartas = []#usuario
+
+#cartas do crupier
+while len(c_cartas) != 2:
+    c_cartas.append(random.randint(1,11))
+    if len(c_cartas) == 2:
+        print ('O crupier tem:',c_cartas)
 
 
