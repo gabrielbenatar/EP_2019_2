c7aba86860d913100de776f0d06b88cedb45774e
gabriel benatar <gabriel.benatar@beityaacov.com.br>
2019-10-16 10:33:40 -0300

    valor baralho sem a
    criei um baralho e atribui os valores menos o ás

---
+++ EP_Blackjack.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 16 10:13:48 2019
+
+@author: gabrielsalvatorbenatar
+"""
+cartas = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A']
+#já que a probabilidade de sair uma das cartas se só hover uma delas ou se hover 4 de cada uma, seria mais fácil deixar uma de cada
+cartas[0]=1
+cartas[1]=2
+cartas[2]=3
+cartas[4]=5
+cartas[3]=4
+cartas[5]=6
+cartas[6]=7
+cartas[7]=8
+cartas[8]=9
+cartas[9]=10
+cartas[10]=10
+cartas[11]=10
+cartas[12]=10
+
+
+
